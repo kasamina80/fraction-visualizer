f8bc881fdea86a6db687aed61d45632c4a335425
kasamina80 <125703134+kasamina80@users.noreply.github.com>
2024-11-06 12:58:05 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -48,11 +48,11 @@ function App() {
 
   const parseIntOrOne = (str: string) => parseInt(str) >= 1 ? parseInt(str) : 1;
 
-  const coerceToIntOrEmpty = (str: string) => parseInt(str) >= 1 ? parseInt(str) : "";
+  const coerceToIntOrEmpty = (str: string) => parseInt(str) >= 1 ? str : "";
 
   const meterChangeHandler = (i: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
     const targetValue: string = e.target.value;
-    const newMeters = meters.map((meter, j) => ( i === j ? `${coerceToIntOrEmpty(targetValue)}` : meter ));
+    const newMeters = meters.map((meter, j) => ( i === j ? coerceToIntOrEmpty(targetValue) : meter ));
     setMeters(newMeters);
   };
 
